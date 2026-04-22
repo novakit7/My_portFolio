e2f623511d30065638c55d6eb2f3dbe8ff0993e0
ankit verma <ankivermajnva@gmail.com>
2026-04-22 17:34:58 +0530

    contact me is completed

--- src/assets/About.tsx
+++ src/assets/About.tsx
@@ -5,7 +5,16 @@ export default function About() {
     }, []);
   return (
     <div>
-      this is about.
+      <div className="left-cont">
+        <h1><span className="has">/</span>About-me</h1>
+        <h4>Who I am ?</h4>
+        <div>Not perfect, not an expert — just someone who genuinely enjoys building things and learning how they work.
+            I started coding out of curiosity, and over time it became something more — a way to create, solve problems, and express ideas.
+            I like experimenting, trying new things, and sometimes failing — because that’s where real learning happens.
+            Still learning. Still building. Still figuring things out.
+            Feel free to reach out — I’m always open to connecting.
+        </div>
+      </div>
     </div>
   )
 }
--- src/assets/Contact.tsx
+++ src/assets/Contact.tsx
@@ -1,11 +1,54 @@
 import { useEffect } from "react";
+import '../styles/contact.css'
 export default function Contact() {
     useEffect(() => {
             document.title = "Contact | Kit Portfolio";
     }, []);
   return (
-    <div>
-     contact me. 
+    <div className="contact">
+      <div className="left-cont">
+        <h1><span className="has">//</span>Contact-me..</h1>
+        <div className="cont-text">
+          <h4>Got something in mind?</h4>
+          Feel free to reach out — I’m always open to connecting.
+        </div>
+        <div className="all-media">
+          <form action="https://formspree.io/f/xnjlgyjd" method="POST">
+            <input className="text-area" type="text" name="name" required placeholder="Enter Your Name"/>
+            <input className="text-area" type="email" name="email" required placeholder="Enter Your Email"/>
+            <textarea className="text-area" name="message" required placeholder="Write your Message Here......"style={{height: "150px"}}></textarea>
+            <button className="text-btn" type="submit">Start a Conversation</button>
+          </form>
+        </div>
+      </div>
+      <div className="right-cont">
+        <div className="suppot">
+          <div className="card1">
+            <h6>Support Me Here</h6>
+            <ul>
+              <li>
+              <a title="LinkedIn - connect me" href="https://www.linkedin.com/in/ankit-verma-04a3051b7/"
+               target="_blank" rel="noopener noreferrer" className="c-links"><i className="fa-brands fa-square-linkedin"/>Linked-In</a></li>
+              <li>
+                <a title="GitHub - view my work" href="https://github.com/novakit7" target="_blank"
+                rel="noopener noreferrer" className="c-links"><i className="fa-brands fa-github"/>GitHub</a></li>
+              <li>
+                <a title="LeetCode - coding platform" href="https://leetcode.com/u/novakit/" target="_blank"
+                rel="noopener noreferrer" className="c-links"><i className="fa-solid fa-laptop-code"/>Leetcode</a></li>
+              <li>
+                <a title="codeforces - coding platform" href="https://codeforces.com/profile/ankit_verma07" target="_blank"
+                rel="noopener noreferrer" className="c-links"><i className="fa-solid fa-code"/>Codeforces</a></li>
+            </ul>
+          </div>
+          <div className="card1">
+            <h6>Contact me Here</h6>
+              <ul>
+                <li><i className="fa-brands fa-whatsapp"/>+918299097314</li>
+                <li><i className="fa-brands fa-envelope"/>ankivermajnva@gmail.com</li>
+              </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
